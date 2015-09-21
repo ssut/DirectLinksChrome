6dc691d1400cbfbca1ad945e831c43db441da989
ssut <ssut@ssut.me>
2015-09-21 21:44:44 +0900

    Add support for Google

--- directlinks.user.js
+++ directlinks.user.js
@@ -16,10 +16,21 @@ var dlinks = {
         urlpattern: 'google.',
         pattern: /[?&]url=[^&]+/,
         replaceTo: function(link) {
-            return link;
-        },
-        checkValid: function(hostname, param) {
+            if(link.getAttribute('onmousedown')) {
+                link.removeAttribute('onmousedown');
+            }
 
+            if(link.pathname === '/url') {
+                if ((/[?&]url=[^&]+/).test(link.search)) {
+                    return decodeURIComponent(link.search.split(/[?&]url=/)[1].split('&')[0]);
+                }
+            }
+
+            return null;
+        },
+        checkValid: function(hostname, pathname, fullpath) {
+            return (hostname.indexOf('www.') > -1 || hostname.indexOf('encrypted') > -1) &&
+                   (pathname === '/' || pathname === '/search' || pathname === '/webhp');
         }
     }
 };
@@ -33,17 +44,22 @@ function onClick(e) {
     if(!et || !et.href) return;
     var link = et;
     e.stopPropagation();
-    console.log(link);
+    var newlink = match.replaceTo(link);
+    if(newlink) link.href = newlink;
 }
 
 function execute() {
-    var hostname = window.location.hostname;
+    var hostname = window.location.hostname,
+        pathname = window.location.pathname,
+        param = [window.location.search, window.location.hash].join('');
     for(var name in dlinks) {
         var rule = dlinks[name];
         if(hostname.indexOf(rule.urlpattern) > -1) {
-            match = rule;
-            document.addEventListener('mousedown', onClick, false);
-            break;
+            if(rule.checkValid(hostname, pathname, param)) {
+                match = rule;
+                document.addEventListener('mousedown', onClick, false);
+                break;
+            }
         }
     }
 }
