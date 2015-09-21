f943f83ee6e1e935bf86528a07057ac324b4ef56
ssut <ssut@ssut.me>
2015-09-21 19:52:28 +0900

    initialize script

---
+++ directlinks.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name DirectLinks
+// @namespace http://ssut.me/directlinks
+// @version 0.1.0
+// @description Do not track what I do on a website
+// @updateURL https://raw.githubusercontent.com/ssut/DirectLinksChrome/stable/directlinks.user.js
+// @downloadURL https://raw.githubusercontent.com/ssut/DirectLinksChrome/stable/directlinks.user.js
+// Targets
+// @include *://*.google*
+// @copyright 2015 SuHun Han (ssut)
+// @grant none
+// ==/UserScript==
